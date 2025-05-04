04fcfb2ad4f2508351f2894306f44d36edbe841e
Mo0rBy <will.moorby2@gmail.com>
2025-05-04 08:44:16 +0100

    Re-arranged ftplugin/ and after/ftplugin/ directories

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,10 +1,1 @@
-local opt = vim.opt_local
-
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.softtabstop = 4
-
-opt.wrap = true
-opt.linebreak = true
-
 vim.opt_local.conceallevel = 2 -- required for obsidian.nvim plugin to correctly render things
