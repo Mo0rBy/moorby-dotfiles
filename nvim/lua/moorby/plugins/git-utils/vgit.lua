0cb4332158fe6c3cfffd1b2bbf0890b535fe8678
Mo0rBy <will.moorby2@gmail.com>
2025-06-15 11:39:26 +0100

    Adding VGit plugin keymappings - remove NeoGit plugin and Telescope git branches keymap

--- nvim/lua/moorby/plugins/git-utils/vgit.lua
+++ nvim/lua/moorby/plugins/git-utils/vgit.lua
@@ -40,15 +40,23 @@ return {
     })
 
     local keymap = vim.keymap
+    keymap.set("n", "gdp", function() vgit.project_diff_preview() end, {desc = "Git diff preview of project"})
+    keymap.set("n", "glp", function() vgit.project_logs_preview() end, {desc = "Git logs view of project"})
+
     keymap.set("n", "gk", function() vgit.hunk_up() end, {desc = "Move to next hunk above the cursor"})
     keymap.set("n", "gj", function() vgit.hunk_down() end, {desc = "Move to next hunk below the cursor"})
-    keymap.set("n", "gs", function() vgit.buffer_hunk_stage() end, {desc = "Stage hunk"})
-    keymap.set("n", "gr", function() vgit.buffer_hunk_reset() end, {desc = "Reset hunk"})
-    keymap.set("n", "gp", function() vgit.buffer_hunk_preview() end, {desc = "Preview hunk"})
-    keymap.set("n", "gb", function() vgit.buffer_blame_preview() end, {desc = "Buffer blame preview"})
-    keymap.set("n", "gf", function() vgit.buffer_diff_preview() end, {desc = "Buffer diff preview"})
-    keymap.set("n", "gh", function() vgit.buffer_history_preview() end, {desc = "Buffer history preview"})
-    keymap.set("n", "gu", function() vgit.buffer_reset() end, {desc = "Buffer reset"})
-    keymap.set("n", "gd", function() vgit.project_diff_preview() end, {desc = "Project diff preview"})
+
+    keymap.set("n", "gsh", function() vgit.buffer_hunk_stage() end, {desc = "Stage hunk"})
+    keymap.set("n", "grh", function() vgit.buffer_hunk_reset() end, {desc = "Reset hunk"})
+    keymap.set("n", "gph", function() vgit.buffer_hunk_preview() end, {desc = "Preview hunk"})
+
+    keymap.set("n", "gbb", function() vgit.buffer_blame_preview() end, {desc = "Buffer blame preview"})
+    keymap.set("n", "gdb", function() vgit.buffer_diff_preview() end, {desc = "Buffer diff preview"})
+    keymap.set("n", "gbh", function() vgit.buffer_history_preview() end, {desc = "Buffer history preview"})
+    keymap.set("n", "gbr", function() vgit.buffer_reset() end, {desc = "Buffer reset"})
+
+    keymap.set("n", "gcb", function() vgit.buffer_conflict_accept_both() end, {desc = "Buffer conflict accept both"})
+    keymap.set("n", "gcc", function() vgit.buffer_conflict_accept_current() end, {desc = "Buffer conflict accept current"})
+    keymap.set("n", "gci", function() vgit.buffer_conflict_accept_incoming() end, {desc = "Buffer conflict accept incoming"})
   end,
 }
