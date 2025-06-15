df89bbf90b1205de0de26e40438f15bdfcfc843c
Mo0rBy <will.moorby2@gmail.com>
2025-06-15 10:05:24 +0100

    Updating the formatting for LSP keymapping definitions

--- nvim/lua/moorby/plugins/lsp/lspconfig.lua
+++ nvim/lua/moorby/plugins/lsp/lspconfig.lua
@@ -63,45 +63,19 @@ return {
       opts.buffer = bufnr
 
       -- set keymaps
-      opts.desc = "Show LSP references"
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-      opts.desc = "Go to declaration"
-      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-      opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-      opts.desc = "Show LSP implementations"
-      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-      opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-      opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-      vim.keymap.set("n", "<leader>rn", function()
-        return ":IncRename " .. vim.fn.expand("<cword>")
-      end, { desc = "Smart rename", expr = true }) -- smart rename (depends on inc-rename plugin)
-
-      opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-      opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-      opts.desc = "Go to previous diagnostic"
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-      opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-      opts.desc = "Show documentation for what is under cursor"
-      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-      opts.desc = "Restart LSP"
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      keymap.set("n", "<leader>lR", "<cmd>Telescope lsp_references<CR>", {desc = "Show LSP references"})
+      keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, {desc = "Go to declaration"})
+      keymap.set("n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", {desc = "Show LSP definitions"})
+      keymap.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", {desc = "Show LSP implementations"})
+      keymap.set("n", "<leader>lt", "<cmd>Telescope lsp_type_definitions<CR>", {desc = "Show LSP type definitions"})
+      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", {desc = "Show buffer diagnostics"})
+      keymap.set("n", "<leader>d", vim.diagnostic.open_float, {desc = "Show line diagnostics"})
+      keymap.set("n", "[d", vim.diagnostic.goto_prev, {desc = "Go to previous diagnostics"})
+      keymap.set("n", "]d", vim.diagnostic.goto_next, {desc = "Go to next diagnostic"})
+      keymap.set("n", "K", vim.lsp.buf.hover, {desc = "Show documentation for what is under the cursor"})
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {desc = "See available code actions"}) -- in visual mode, this will apply to the current selection
+      keymap.set("n", "<leader>rn", function() return ":IncRename " .. vim.fn.expand("<cword>") end, { desc = "Smart rename", expr = true }) -- depends on inc-rename plugin
+      keymap.set("n", "<leader>rs", ":LspRestart<CR>", {desc = "Restart LSP"})
     end
 
     -- used to enable autocompletion (assign to every lsp server config)
