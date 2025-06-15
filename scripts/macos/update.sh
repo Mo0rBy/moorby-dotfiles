50cb2640493b5016cc3b303ab8f54e519a4b5ba5
Mo0rBy <will.moorby2@gmail.com>
2025-06-15 07:02:43 +0100

    Added scripts/macos/update.sh

---
+++ scripts/macos/update.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+echo "--- BREW UPDATE ---"
+brew update
+echo "--- BREW UPGRADE ---"
+brew upgrade
+
+echo "--- NVIM LAZY SYNC ---"
+nvim --headless "+Lazy! sync" +qa
+
+echo "--- MACOS UPGRADE ---"
+softwareupdate -ia
