2eb9262b7bf91aaea7311c1f81d46c5da561c816
Mo0rBy <will.moorby2@gmail.com>
2025-11-29 17:16:03 +0000

    Update lspconfig settings - migrate to new way of setting up lsp in nvim introduced in 0.11 and update ensure_installed list

--- nvim/lua/moorby/plugins/lsp/mason.nvim.lua
+++ nvim/lua/moorby/plugins/lsp/mason.nvim.lua
@@ -28,20 +28,16 @@ return {
         "gopls",
         "groovyls",
         "html",
-        -- "htmx", -- leaving this out for now as it does not install correctly and it looks to be a very new LSP server
         "helm_ls",
         "jsonls",
         "java_language_server",
-        "quick_lint_js",
         "ts_ls",
-        "jqls",
         "lua_ls",
         "marksman",
         "pyright",
         "terraformls",
         "tflint",
-        "lemminx",
-        "yamlls"
+        "yamlls",
       },
       automatic_installation = false,  -- do not auto-install LSP servers for languages not already installed when relevant file type is opened
       automatic_enable = false
