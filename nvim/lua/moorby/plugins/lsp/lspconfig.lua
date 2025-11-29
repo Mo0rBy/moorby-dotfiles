e236f37f572d35caa064486dc552cba31eea3fe2
Mo0rBy <will.moorby2@gmail.com>
2025-11-29 17:16:03 +0000

    Update lspconfig settings - migrate to new way of setting up lsp in nvim introduced in 0.11 and update ensure_installed list

--- nvim/lua/moorby/plugins/lsp/lspconfig.lua
+++ nvim/lua/moorby/plugins/lsp/lspconfig.lua
@@ -8,7 +8,6 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
     -- copy ensure_installed list from mason.lua
     -- this will be used to setup all the LSP's that are installed
@@ -18,24 +17,19 @@ return {
       "dockerls",
       "docker_compose_language_service",
       "eslint",
-      "flux_lsp",
       "gopls",
       "groovyls",
       "html",
-      -- "htmx",
       "helm_ls",
       "jsonls",
       "java_language_server",
-      "quick_lint_js",
       "ts_ls",
-      "jqls",
       "lua_ls",
       "marksman",
       "pyright",
       "terraformls",
       "tflint",
-      "lemminx",
-      "yamlls"
+      "yamlls",
     }
 
     vim.diagnostic.config({
@@ -57,35 +51,39 @@ return {
 
     local keymap = vim.keymap
 
-    local opts = { noremap = true, silent = true }
-    -- the on_attach function will execute when attached to new buffers that the LSP server starts on
-    local on_attach = function(client, buffer)
-      opts.buffer = bufnr
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function()
+        -- set keymaps
+        keymap.set("n", "<leader>lR", "<cmd>Telescope lsp_references<CR>", { desc = "Show LSP references", noremap = true, silent = true })
+        keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, { desc = "Go to declaration", noremap = true, silent = true })
+        keymap.set("n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", {desc = "Show LSP definitions", noremap = true, silent = true })
+        keymap.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", { desc = "Show LSP implementations", noremap = true, silent = true })
+        keymap.set("n", "<leader>lt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "Show LSP type definitions", noremap = true, silent = true })
+        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "Show buffer diagnostics", noremap = true, silent = true })
+        keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics", noremap = true, silent = true })
+        keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous diagnostics", noremap = true, silent = true })
+        keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next diagnostic", noremap = true, silent = true })
+        keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation for what is under the cursor", noremap = true, silent = true })
+        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "See available code actions", noremap = true, silent = true }) -- in visual mode, this will apply to the current selection
+        keymap.set("n", "<leader>rn", function() return ":IncRename " .. vim.fn.expand("<cword>") end, { desc = "Smart rename", expr = true , noremap = true, silent = true }) -- depends on inc-rename plugin
+        keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart LSP", noremap = true, silent = true })
+      end,
+    })
 
-      -- set keymaps
-      keymap.set("n", "<leader>lR", "<cmd>Telescope lsp_references<CR>", {desc = "Show LSP references"})
-      keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, {desc = "Go to declaration"})
-      keymap.set("n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", {desc = "Show LSP definitions"})
-      keymap.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", {desc = "Show LSP implementations"})
-      keymap.set("n", "<leader>lt", "<cmd>Telescope lsp_type_definitions<CR>", {desc = "Show LSP type definitions"})
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", {desc = "Show buffer diagnostics"})
-      keymap.set("n", "<leader>d", vim.diagnostic.open_float, {desc = "Show line diagnostics"})
-      keymap.set("n", "[d", vim.diagnostic.goto_prev, {desc = "Go to previous diagnostics"})
-      keymap.set("n", "]d", vim.diagnostic.goto_next, {desc = "Go to next diagnostic"})
-      keymap.set("n", "K", vim.lsp.buf.hover, {desc = "Show documentation for what is under the cursor"})
-      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {desc = "See available code actions"}) -- in visual mode, this will apply to the current selection
-      keymap.set("n", "<leader>rn", function() return ":IncRename " .. vim.fn.expand("<cword>") end, { desc = "Smart rename", expr = true }) -- depends on inc-rename plugin
-      keymap.set("n", "<leader>rs", ":LspRestart<CR>", {desc = "Restart LSP"})
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local has_cmp_nvim_lsp, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+    if has_cmp_nvim_lsp then
+      capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
     end
 
-    -- used to enable autocompletion (assign to every lsp server config)
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+    vim.lsp.config('*', {
+      capabilities = capabilities
+    })
 
+    -- add any LSP's to this list if the need additional or specific settings
     for _, lsp in pairs(ensure_installed) do
-      if (lsp == "lua_ls") then -- configure lua_ls with special settings
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+      if (lsp == "lua_ls") then
+        vim.lsp.config(lsp, {
           settings = {
             Lua = {
               diagnostics = {
@@ -101,10 +99,8 @@ return {
           }
         })
 
-      elseif (lsp == "helm_ls") then -- configure helm_ls with special settings
-        lspconfig["helm_ls"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+      elseif (lsp == "helm_ls") then
+        vim.lsp.config(lsp, {
           settings = {
             ["helm-ls"] = {
               yamlls = {
@@ -114,27 +110,19 @@ return {
           }
         })
 
-      elseif (lsp == "groovyls") then -- configure groovyls with special settings
-        lspconfig["groovyls"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+      elseif (lsp == "groovyls") then
+        vim.lsp.config(lsp, {
           cmd = { "groovy-language-server" },
         })
 
-      elseif (lsp == "java_language_server") then -- configure java_language_server with special settings
-        lspconfig["java_language_server"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+      elseif (lsp == "java_language_server") then
+        vim.lsp.config(lsp, {
           cmd = { "java-language-server" },
         })
 
-
-      else -- add more conditionals for other LSP's if required
-        lspconfig[lsp].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
       end
     end
+
+    vim.lsp.enable(ensure_installed)
   end
 }
