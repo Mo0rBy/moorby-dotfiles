9e4811542f49309777f6abe255fb2cc96b507fb8
Mo0rBy <will.moorby2@gmail.com>
2025-11-30 14:33:21 +0000

    udpate blame.nvim plugin (used for git blame on whole file) to load on keymap

--- nvim/lua/moorby/plugins/git/blame.nvim.lua
+++ nvim/lua/moorby/plugins/git/blame.nvim.lua
@@ -1,15 +1,12 @@
 return {
   "FabijanZulj/blame.nvim",
-  lazy = false,
+  keys = {
+    { "<leader>gfb", ":BlameToggle<CR>", mode = "n", desc = "Toggle git blame (entire file)" }
+  },
   config = function()
-    -- Setup plugin
     require("blame").setup({
       merge_consecutive = true,
       date_format = "%d/%m/%Y %H:%M"
     })
-
-    -- Setup keymaps
-    local keymap = vim.keymap
-    keymap.set("n", "<leader>gfb", ":BlameToggle<CR>", {desc = "Toggle git blame (entire file)"})
   end
 }
