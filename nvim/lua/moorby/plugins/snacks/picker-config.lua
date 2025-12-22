0f81f82c4b913b54cabf8a4daa463a70ef7f5544
Mo0rBy <will.moorby2@gmail.com>
2025-12-22 16:38:36 +0000

    update snacks picker config with custom_default layout with height and width of 0.95

--- nvim/lua/moorby/plugins/snacks/picker-config.lua
+++ nvim/lua/moorby/plugins/snacks/picker-config.lua
@@ -1,6 +1,32 @@
 return {
   enabled = true,
-  frecency = true,
+  layouts = {
+    custom_default = {
+      -- same as the 'default' layout but adjusted width and height values
+      layout = {
+        box = "horizontal",
+        width = 0.95,
+        min_width = 120,
+        height = 0.95,
+        {
+          box = "vertical",
+          border = true,
+          title = "{title} {live} {flags}",
+          { win = "input", height = 1, border = "bottom" },
+          { win = "list", border = "none" },
+        },
+        { win = "preview", title = "{preview}", border = true, width = 0.5 },
+      }
+    }
+  },
+  layout = {
+    preset = function()
+      return vim.o.columns >= 120 and "custom_default" or "vertical"
+    end,
+  },
+  matcher = {
+    frecency = true,
+  },
   formatters = {
     file = {
       truncate = "left"
