8410ccaf8cde1430a0179ca3350b0484879dff44
Mo0rBy <will.moorby2@gmail.com>
2025-12-22 15:30:13 +0000

    segregate custom configs for snacks modules

--- nvim/lua/moorby/plugins/snacks.nvim.lua
+++ nvim/lua/moorby/plugins/snacks.nvim.lua
@@ -3,45 +3,14 @@ return {
   lazy = false,
   priority = 900,
   opts = {
-    dashboard = {
-      enabled = true,
-      preset = {
-        header = [[
-    __  ___      ____       ____       
-   /  |/  /___  / __ \_____/ __ )__  __
-  / /|_/ / __ \/ / / / ___/ __  / / / /
- / /  / / /_/ / /_/ / /  / /_/ / /_/ / 
-/_/  /_/\____/\____/_/  /_____/\__, /  
-                          /____/]],
-        keys = {
-          { icon = "", desc = "New file", key = "e", action = ":ene | startinsert" },
-          { icon = "󰈞", desc = "Find file", key = "SPC ff", action = function() Snacks.picker.files() end },
-          { icon = "", desc = "Find grep", key = "SPC fg", action = function() Snacks.picker.grep() end },
-          { icon = "", desc = "Recent files", key = "SPC fr", action = function() Snacks.picker.recent() end },
-          { icon = "", desc = "Quit", key = "q", action = ":qa" }
-        }
-      },
-      sections = {
-        { section = "header" },
-        { section = "keys", gap = 1, padding = 1 },
-        { section = "startup" },
-      },
-    },
+    dashboard = require("moorby.plugins.snacks.dashboard-config"),
     input = {
       enabled = true
     },
     lazygit = {
       enabled = true
     },
-    picker = {
-      enabled = true,
-      frecency = true,
-      formatters = {
-        file = {
-          truncate = "left"
-        }
-      }
-    },
+    picker = require("moorby.plugins.snacks.picker-config"),
     statuscolumn = {
       enabled = true
     }
---
+++ nvim/lua/moorby/plugins/snacks/picker-config.lua
@@ -0,0 +1,9 @@
+return {
+  enabled = true,
+  frecency = true,
+  formatters = {
+    file = {
+      truncate = "left"
+    }
+  }
+}
